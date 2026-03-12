006459edb4f9f0923b27338316e1c16cd0e1007e
Sébastien <sebvermandele@gmail.com>
2026-03-12 17:18:22 -0400

    Initial Commit

---
+++ cani/caniApp.swift
@@ -0,0 +1,17 @@
+//
+//  caniApp.swift
+//  cani
+//
+//  Created by Sébastien Vermandele on 2026-03-12.
+//
+
+import SwiftUI
+
+@main
+struct caniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
